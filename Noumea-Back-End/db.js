cffcbc59902d9128d331301cb69088ee8f176722
Mehdi Rhoulam <mehdi.rhoulam@epitech.eu>
2024-04-20 12:48:00 +0200

    direct database url

--- Noumea-Back-End/db.js
+++ Noumea-Back-End/db.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = "mongodb+srv://dietis13008:Yw0MgWYsTmO77EDO@cluster0.yvbp8vj.mongodb.net/noumeaBnB?retryWrites=true&w=majority&appName=Cluster0"; //process.env.MONGO_URI;
 
 const connectDB = async () => {
     try {
